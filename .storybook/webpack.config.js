496f45771ea7c3c94e094ea318351a58a4fdb6de
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2018-12-18 16:07:58 +0900

    Make the storybook settings independent

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,62 +1,192 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 
 const appDirectory = fs.realpathSync(process.cwd());
-const craConfig = require('../config/webpack.config.dev');
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-module.exports = (baseConfig, env, config) => {
-  const craConfigOneOf = craConfig.module.rules[2].oneOf;
+const paths = {
+  appPath: resolveApp('.'),
+  appTsConfig: resolveApp('tsconfig.json'),
+  appNodeModules: resolveApp('node_modules'),
+};
 
-  // Use rules of webpack.dev.config that created react-create-app,
-  config.module.rules = [
+const moduleFileExtensions = [
+  'web.mjs',
+  'mjs',
+  'web.js',
+  'js',
+  'web.ts',
+  'ts',
+  'web.tsx',
+  'tsx',
+  'json',
+  'web.jsx',
+  'jsx',
+];
+
+// Check if TypeScript is setup
+const useTypeScript = fs.existsSync(paths.appTsConfig);
+
+// style files regexes
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+// common function to get style loaders
+const getStyleLoaders = (cssOptions, preProcessor) => {
+  const loaders = [
+    require.resolve('style-loader'),
     {
-      oneOf: [
-        craConfigOneOf[0], // url loader
-        {
-          test: /\.(js|mjs|jsx|ts|tsx)$/,
-          // Include src and stories directories.
-          include: path.resolve(appDirectory),
-          exclude: path.resolve(appDirectory, 'node_modules'),
-          loader: require.resolve('babel-loader'),
-          options: {
-            customize: require.resolve(
-              'babel-preset-react-app/webpack-overrides'
-            ),            
-            plugins: [
-              [
-                require.resolve('babel-plugin-named-asset-import'),
-                {
-                  loaderMap: {
-                    svg: {
-                      ReactComponent: '@svgr/webpack?-prettier,-svgo![path]',
-                    },
-                  },
-                },
-              ],
-            ],
-            cacheDirectory: true,
-            cacheCompression: false,
-          },
-        },
-        // JS outside of the app with Babel
-        craConfigOneOf[2], 
-        // postcss loader
-        craConfigOneOf[3], 
-        // CSS Modules
-        craConfigOneOf[4], 
-        {
-          // Add ejs to exclude params.
-          exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.ejs$/],
-          loader: require.resolve('file-loader'),
-          options: {
-            name: 'static/media/[name].[hash:8].[ext]',
-          },
-        }
-      ],
+      loader: require.resolve('css-loader'),
+      options: cssOptions,
+    },
+    {
+      // Options for PostCSS as we reference these options twice
+      // Adds vendor prefixing based on your specified browser support in
+      // package.json
+      loader: require.resolve('postcss-loader'),
+      options: {
+        // Necessary for external CSS imports to work
+        // https://github.com/facebook/create-react-app/issues/2677
+        ident: 'postcss',
+        plugins: () => [
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            features: {
+              'color-mod-function': true,
+            },
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 0,
+            importFrom: path.resolve('src/variables.css'),
+          }),
+        ],
+      },
     },
   ];
+  if (preProcessor) {
+    loaders.push(require.resolve(preProcessor));
+  }
+  return loaders;
+};
 
-  config.resolve.extensions = craConfig.resolve.extensions;
+const resolveExtensins = moduleFileExtensions
+  .map(ext => `.${ext}`)
+  .filter(ext => useTypeScript || !ext.includes('ts'));
+
+const moduleRules = [
+  {
+    oneOf: [
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        // Include src and stories directories.
+        include: paths.appPath,
+        exclude: paths.appNodeModules,
+        loader: require.resolve('babel-loader'),
+        options: {
+          customize: require.resolve(
+            'babel-preset-react-app/webpack-overrides'
+          ),            
+          plugins: [
+            [
+              require.resolve('babel-plugin-named-asset-import'),
+              {
+                loaderMap: {
+                  svg: {
+                    ReactComponent: '@svgr/webpack?-prettier,-svgo![path]',
+                  },
+                },
+              },
+            ],
+          ],
+          cacheDirectory: true,
+          cacheCompression: false,
+        },
+      },
+      {
+        test: /\.(js|mjs)$/,
+        exclude: /@babel(?:\/|\\{1,2})runtime/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          babelrc: false,
+          configFile: false,
+          compact: false,
+          presets: [
+            [
+              require.resolve('babel-preset-react-app/dependencies'),
+              { helpers: true },
+            ],
+          ],
+          cacheDirectory: true,
+          // Don't waste time on Gzipping the cache
+          cacheCompression: false,
+          
+          // If an error happens in a package, it's possible to be
+          // because it was compiled. Thus, we don't want the browser
+          // debugger to show the original code. Instead, the code
+          // being evaluated would be much more helpful.
+          sourceMaps: false,
+        },
+      },
+      {
+        test: cssRegex,
+        exclude: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1,
+        }),
+      },
+      {
+        test: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1,
+          modules: true,
+          getLocalIdent: getCSSModuleLocalIdent,
+        }),
+      },
+      {
+        test: sassRegex,
+        exclude: sassModuleRegex,
+        use: getStyleLoaders({ importLoaders: 2 }, 'sass-loader'),
+      },
+      {
+        test: sassModuleRegex,
+        use: getStyleLoaders(
+          {
+            importLoaders: 2,
+            modules: true,
+            getLocalIdent: getCSSModuleLocalIdent,
+          },
+          'sass-loader'
+        ),
+      },
+      {
+        // Add ejs to exclude params.
+        exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.ejs$/],
+        loader: require.resolve('file-loader'),
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+];
+
+module.exports = (baseConfig, env, config) => {
+  config.module.rules = moduleRules;
+  config.resolve.extensions = resolveExtensins;
 
   return config;
 };
