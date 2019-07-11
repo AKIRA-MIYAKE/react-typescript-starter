753d60a9777853994658aa176fccf86988b524bc
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2019-07-11 14:41:08 +0900

    add linter script with prettier

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -10,12 +10,17 @@ import '../base.css';
 
 import App from '../App';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Button, Welcome } = require('@storybook/react/demo');
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
