de36dae1f74456dc4eb906413e112264b9578048
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2019-11-06 18:51:26 +0900

    Update dependencies

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,11 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 
+const postcssNormalize = require('postcss-normalize');
+
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 const paths = {
   appPath: resolveApp('.'),
+  appSrc: resolveApp('src'),
   appTsConfig: resolveApp('tsconfig.json'),
   appNodeModules: resolveApp('node_modules'),
 };
@@ -36,10 +39,16 @@ const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
+let isEnvDevelopment = true;
+let isEnvProduction = false;
+
 // common function to get style loaders
 const getStyleLoaders = (cssOptions, preProcessor) => {
   const loaders = [
-    require.resolve('style-loader'),
+    isEnvDevelopment && require.resolve('style-loader'),
+    isEnvProduction && {
+      loader: MiniCssExtractPlugin.loader,
+    },
     {
       loader: require.resolve('css-loader'),
       options: cssOptions,
@@ -63,14 +72,32 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
               flexbox: 'no-2009',
             },
             stage: 0,
-            importFrom: path.resolve('src/variables.css'),
+            importFrom: path.resolve(paths.appSrc, './variables.css'),
           }),
+          // Adds PostCSS Normalize as the reset css with default options,
+          // so that it honors browserslist config in package.json
+          // which in turn let's users customize the target behavior as per their needs.
+          postcssNormalize(),
         ],
+        sourceMap: isEnvProduction,
       },
     },
-  ];
+  ].filter(Boolean);
   if (preProcessor) {
-    loaders.push(require.resolve(preProcessor));
+    loaders.push(
+      {
+        loader: require.resolve('resolve-url-loader'),
+        options: {
+          sourceMap: isEnvProduction,
+        },
+      },
+      {
+        loader: require.resolve(preProcessor),
+        options: {
+          sourceMap: true,
+        },
+      }
+    );
   }
   return loaders;
 };
@@ -81,41 +108,56 @@ const resolveExtensins = moduleFileExtensions
 
 const moduleRules = [
   {
+    // "oneOf" will traverse all following loaders until one will
+    // match the requirements. When no loader matches it will fall
+    // back to the "file" loader at the end of the loader list.
     oneOf: [
+      // "url" loader works like "file" loader except that it embeds assets
+      // smaller than specified limit in bytes as data URLs to avoid requests.
+      // A missing `test` is equivalent to a match.
       {
         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
         loader: require.resolve('url-loader'),
         options: {
-          limit: 10000,
+          limit: '10000',
           name: 'static/media/[name].[hash:8].[ext]',
         },
       },
+      // Process application JS with Babel.
+      // The preset includes JSX, Flow, TypeScript, and some ESnext features.
       {
         test: /\.(js|mjs|jsx|ts|tsx)$/,
-        // Include src and stories directories.
-        include: paths.appPath,
-        exclude: paths.appNodeModules,
+        include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
           customize: require.resolve(
             'babel-preset-react-app/webpack-overrides'
-          ),            
+          ),
+
           plugins: [
             [
               require.resolve('babel-plugin-named-asset-import'),
               {
                 loaderMap: {
                   svg: {
-                    ReactComponent: '@svgr/webpack?-prettier,-svgo![path]',
+                    ReactComponent:
+                      '@svgr/webpack?-svgo,+titleProp,+ref![path]',
                   },
                 },
               },
             ],
           ],
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
           cacheDirectory: true,
+          // See #6846 for context on why cacheCompression is disabled
           cacheCompression: false,
+          compact: isEnvProduction,
         },
       },
+      // Process any JS outside of the app with Babel.
+      // Unlike the application JS, we only compile the standard ES features.
       {
         test: /\.(js|mjs)$/,
         exclude: /@babel(?:\/|\\{1,2})runtime/,
@@ -131,9 +173,9 @@ const moduleRules = [
             ],
           ],
           cacheDirectory: true,
-          // Don't waste time on Gzipping the cache
+          // See #6846 for context on why cacheCompression is disabled
           cacheCompression: false,
-          
+
           // If an error happens in a package, it's possible to be
           // because it was compiled. Thus, we don't want the browser
           // debugger to show the original code. Instead, the code
@@ -141,52 +183,105 @@ const moduleRules = [
           sourceMaps: false,
         },
       },
+      // "postcss" loader applies autoprefixer to our CSS.
+      // "css" loader resolves paths in CSS and adds assets as dependencies.
+      // "style" loader turns CSS into JS modules that inject <style> tags.
+      // In production, we use MiniCSSExtractPlugin to extract that CSS
+      // to a file, but in development "style" loader enables hot editing
+      // of CSS.
+      // By default we support CSS Modules with the extension .module.css
       {
         test: cssRegex,
         exclude: cssModuleRegex,
         use: getStyleLoaders({
           importLoaders: 1,
+          sourceMap: isEnvProduction,
         }),
+        // Don't consider CSS imports dead code even if the
+        // containing package claims to have no side effects.
+        // Remove this when webpack adds a warning or an error for this.
+        // See https://github.com/webpack/webpack/issues/6571
+        sideEffects: true,
       },
+      // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+      // using the extension .module.css
       {
         test: cssModuleRegex,
         use: getStyleLoaders({
           importLoaders: 1,
+          sourceMap: isEnvProduction,
           modules: true,
           getLocalIdent: getCSSModuleLocalIdent,
         }),
       },
+      // Opt-in support for SASS (using .scss or .sass extensions).
+      // By default we support SASS Modules with the
+      // extensions .module.scss or .module.sass
       {
         test: sassRegex,
         exclude: sassModuleRegex,
-        use: getStyleLoaders({ importLoaders: 2 }, 'sass-loader'),
+        use: getStyleLoaders(
+          {
+            importLoaders: 2,
+            sourceMap: isEnvProduction,
+          },
+          'sass-loader'
+        ),
+        // Don't consider CSS imports dead code even if the
+        // containing package claims to have no side effects.
+        // Remove this when webpack adds a warning or an error for this.
+        // See https://github.com/webpack/webpack/issues/6571
+        sideEffects: true,
       },
+      // Adds support for CSS Modules, but using SASS
+      // using the extension .module.scss or .module.sass
       {
         test: sassModuleRegex,
         use: getStyleLoaders(
           {
             importLoaders: 2,
+            sourceMap: isEnvProduction,
             modules: true,
             getLocalIdent: getCSSModuleLocalIdent,
           },
           'sass-loader'
         ),
       },
+      // "file" loader makes sure those assets get served by WebpackDevServer.
+      // When you `import` an asset, you get its (virtual) filename.
+      // In production, they would get copied to the `build` folder.
+      // This loader doesn't use a "test" so it will catch all modules
+      // that fall through the other loaders.
       {
+        loader: require.resolve('file-loader'),
+        // Exclude `js` files to keep "css" loader working as it injects
+        // its runtime that would otherwise be processed through "file" loader.
+        // Also exclude `html` and `json` extensions so they get processed
+        // by webpacks internal loaders.
         // Add ejs to exclude params.
         exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.ejs$/],
-        loader: require.resolve('file-loader'),
         options: {
           name: 'static/media/[name].[hash:8].[ext]',
         },
       },
+      // ** STOP ** Are you adding a new loader?
+      // Make sure to add the new loader(s) before the "file" loader.
     ],
   },
 ];
 
-module.exports = (baseConfig, env, config) => {
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+  if (mode === 'PRODUCTION') {
+    isEnvDevelopment = false;
+    isEnvProduction = true;
+  }
+
   config.module.rules = moduleRules;
   config.resolve.extensions = resolveExtensins;
 
+  // Return the altered config
   return config;
 };
